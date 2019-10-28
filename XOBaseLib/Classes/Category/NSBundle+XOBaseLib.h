ccf98978cf7f25d19ed3526ee9a356ab2024082a
kenter <Hjt_830@163.com>
2019-10-28 13:42:11 +0800

    XOLocalizedString定义放到NSBundle (XOBaseLib)中

--- XOBaseLib/Classes/Category/NSBundle+XOBaseLib.h
+++ XOBaseLib/Classes/Category/NSBundle+XOBaseLib.h
@@ -10,6 +10,8 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+#define XOLocalizedString(key) ([NSBundle xo_localizedStringForKey:(key)])
+
 @interface NSBundle (XOBaseLib)
 
 + (NSBundle *)xo_baseLibBundle;
--- XOBaseLib/Classes/Config/XOMacro.h
+++ XOBaseLib/Classes/Config/XOMacro.h
@@ -11,7 +11,6 @@
 
 #import <Foundation/Foundation.h>
 
-#define XOLocalizedString(key) ([NSBundle xo_localizedStringForKey:(key)])
 
 #pragma mark ====================== 字体 ======================
 
